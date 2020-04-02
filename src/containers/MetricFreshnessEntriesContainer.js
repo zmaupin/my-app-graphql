f4b16f57b26cde23393fd652cff4dd5626da32cd
Zackary Maupin <zmaupin@gmail.com>
2020-04-02 15:11:42 -0400

    adding lunrjs

--- src/containers/MetricFreshnessEntriesContainer.js
+++ src/containers/MetricFreshnessEntriesContainer.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { MetricFreshnessEntry } from "../components/MetricFreshnessEntry";
 import { GET_METRIC_FRESHNESS_ENTRIES } from "../graphql/get-metric-freshness-entry";
+import { useLunr } from "../common/lunrjs/useLunr";
+import lunr from "lunr";
 
 export function MetricFreshnessEntriesContainer() {
   const { data: { metricFreshnessList = [] } = {} } = useQuery(
@@ -9,12 +11,31 @@ export function MetricFreshnessEntriesContainer() {
     {}
   );
 
+  const store = metricFreshnessList;
+  const index = lunr(function() {
+    this.ref("id");
+    this.field("name");
+    this.field("timestamp");
+
+    store.forEach(function(doc) {
+      this.add(doc);
+    }, this);
+  });
+
+  const [query, setQuery] = useState(null);
+  const results = useLunr(query, index, store);
+
+  const handleQueryChange = event => {
+    event.persist();
+    setQuery(event.target.value);
+  };
+
   return (
     <div className="container">
-      {metricFreshnessList &&
-        metricFreshnessList.map(metricFreshness => (
-          <MetricFreshnessEntry metricFreshnessEntry={metricFreshness} />
-        ))}
+      <input onChange={handleQueryChange} value="Searching..." />;
+      {results.map(result => (
+        <MetricFreshnessEntry metricFreshnessEntry={result} />
+      ))}
     </div>
   );
 }
