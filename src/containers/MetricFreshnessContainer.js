bfcf2e4faca31e4847cfc04ab60c56d5cd0ab954
Zackary Maupin <zmaupin@gmail.com>
2020-04-03 13:35:15 -0400

    only pass query as a dependency

--- src/containers/MetricFreshnessContainer.js
+++ src/containers/MetricFreshnessContainer.js
@@ -1,60 +1,62 @@
 import React, { useState, useEffect } from "react";
-// import { useQuery } from "@apollo/react-hooks";
-// import { MetricFreshnessEntry } from "../components/MetricFreshnessEntry";
-// import { GET_METRIC_FRESHNESS_ENTRIES } from "../graphql/get-metric-freshness-entry";
 import lunr from "lunr";
+import { useQuery } from "@apollo/react-hooks";
+
+import { GET_METRIC_FRESHNESS_ENTRIES } from "../graphql/get-metric-freshness-entry";
 import { MetricFreshnessEntry } from "../components/MetricFreshnessEntry";
 
-const store = [
-  {
-    id: "0",
-    name: "blockstore_allocation_sum",
-    timestamp: "2020-03-16 20:28:33"
-  },
-  {
-    id: "1",
-    name: "ceph_client_io_read_bytes",
-    timestamp: "2020-03-16 20:28:33"
-  },
-  {
-    id: "2",
-    name: "ceph_client_io_read_ops",
-    timestamp: "2020-03-16 20:28:35"
-  }
-];
+// const store = [
+//   {
+//     id: "0",
+//     name: "blockstore_allocation_sum",
+//     timestamp: "2020-03-16 20:28:33"
+//   },
+//   {
+//     id: "1",
+//     name: "ceph_client_io_read_bytes",
+//     timestamp: "2020-03-16 20:28:33"
+//   },
+//   {
+//     id: "2",
+//     name: "ceph_client_io_read_ops",
+//     timestamp: "2020-03-16 20:28:35"
+//   }
+// ];
 
-const index = lunr(function() {
-  this.ref("id");
-  this.field("name");
-  this.field("timestamp");
+// const index = lunr(function() {
+//   this.ref("id");
+//   this.field("name");
+//   this.field("timestamp");
 
-  store.forEach(function(doc) {
-    this.add(doc);
-  }, this);
-});
+//   store.forEach(function(doc) {
+//     this.add(doc);
+//   }, this);
+// });
 
 export function MetricFreshnessContainer() {
-  // const { data: { metricFreshnessList = [] } = {} } = useQuery(
-  //   GET_METRIC_FRESHNESS_ENTRIES,
-  //   {}
-  // );
+  const { data: { metricFreshnessList = [] } = {} } = useQuery(
+    GET_METRIC_FRESHNESS_ENTRIES,
+    {}
+  );
 
-  // const index = lunr(function() {
-  //   this.ref("id");
-  //   this.field("name");
-  //   this.field("timestamp");
+  const index = lunr(function() {
+    this.ref("id");
+    this.field("name");
+    this.field("timestamp");
 
-  //   metricFreshnessList.forEach(function(doc) {
-  //     this.add(doc);
-  //   }, this);
-  // });
+    metricFreshnessList.forEach(function(doc) {
+      this.add(doc);
+    }, this);
+  });
 
   const [query, setQuery] = useState("");
   const [results, setResults] = useState(null);
 
   useEffect(() => {
     const searchResults = index.search(query);
-    const processedResults = searchResults.map(({ ref }) => store[ref]);
+    const processedResults = searchResults.map(
+      ({ ref }) => metricFreshnessList[ref]
+    );
 
     setResults(processedResults);
   }, [query]);
@@ -64,7 +66,8 @@ export function MetricFreshnessContainer() {
     setQuery(event.target.value);
   };
 
-  const entries = query === "" ? store : results;
+  console.log(`results: ${results}`);
+  const entries = query === "" ? metricFreshnessList : results;
 
   return (
     <div className="container">
